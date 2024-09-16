2c21a4530132abb1b58b71097398bd8310020804
FrancoYudica <francoyudica@gmail.com>
2024-09-16 09:30:09 -0300

    Removed unused code

--- tp5-busquedas-locales/code/main.py
+++ tp5-busquedas-locales/code/main.py
@@ -1,7 +1,4 @@
 from chess_board_state import ChessBoardState
-from typing import List
-import random
-import time
 from local_search_algo.local_search_result import LocalSearchResult
 
 from local_search_algo.hill_climb.random_restart_hill_climb import hill_climb
@@ -49,48 +46,4 @@ def random_restart_hill_climb_test(board):
 
 if __name__ == "__main__":
     board = ChessBoardState(8)
-
     hill_climb_success_rate_test(board, 1000)
-    # hill_climb_test(board)
-    # random_restart_hill_climb_test(board)
-
-    # for i in range(iterations):
-
-        
-    #     final_board = hill_climb(
-    #         initial_board=board, 
-    #         maximum_states=30,
-    #         maximum_shoulder_iterations=10)
-        
-    #     if final_board.cached_threats == 0:
-    #         success_count += 1
-
-    # print(f"Success ratio: {success_count / iterations}")
-    # final_board = hill_climb(
-    #     initial_board=board, 
-    #     maximum_states=5,
-    #     maximum_shoulder_iterations=1)
-
-    # if final_board.cached_threats == 0:
-    #     print("Success")
-    # else:
-    #     print("Failure")
-
-    # print(final_board)
-
-    # Random successor test
-    # for i in range(3):
-
-    #     print(f"Successor: {i}")
-    #     successor_board = random_successor_chess_board(board)
-    #     print(successor_board)
-
-    # All successors test
-    # for successor in all_successors(board):
-    #     print("Successor")
-    #     print(successor)
-
-
-    
-
-
