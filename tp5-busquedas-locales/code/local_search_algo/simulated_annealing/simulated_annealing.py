112465d167482a083bb7fd96c17838a4c61d52f8
FrancoYudica <francoyudica@gmail.com>
2024-09-18 12:25:54 -0300

    Improved simulated annealing

--- tp5-busquedas-locales/code/local_search_algo/simulated_annealing/simulated_annealing.py
+++ tp5-busquedas-locales/code/local_search_algo/simulated_annealing/simulated_annealing.py
@@ -5,9 +5,10 @@ import random
 import time
 import math
 
-def schedule(t) -> float:
+def schedule(t, cooling_rate) -> float:
     # Slower cooling schedule
-    return max(1.0 / math.log(t + 1), 1e-10)
+    # return max(1.0 / (math.log(t + 1) * cooling_rate), 1e-10)
+    return 1.0 / (t * cooling_rate)
 
 def simulated_annealing(
         initial_board: ChessBoardState,
@@ -17,63 +18,39 @@ def simulated_annealing(
     search_result = LocalSearchResult()
     t0 = time.time()
     current_board = initial_board
-    traversed_states = 1
-    iterations = 0
-    shoulder_counter = 0
 
-    while traversed_states < maximum_states and iterations < maximum_states * 5:
-        iterations += 1
+    for t in range(1, maximum_states + 1):
 
-        T = schedule(iterations)
-
-        if T == 0:
-            break
-        
         if current_board.cached_threats == 0:
             break
-        
+
         # Computes all the possible successors for the current state
         successor_board = random_successor_chess_board(current_board)
-
-        # If the shoulder maximum count is reached, ensures that the successor
-        # has a different value
-        while shoulder_counter == maximum_shoulder_iterations:
-            if successor_board.cached_threats == current_board.cached_threats:
-                successor_board = random_successor_chess_board(current_board)
-            else:
-                break
 
         delta_e = successor_board.cached_threats - current_board.cached_threats
 
         next_board = None
+        T = schedule(t, 0.5)
 
         # When successor gets closer to local minimum or same (shoulders)
         if delta_e <= 0.0:
             next_board = successor_board
-            # print(f"Picking better {current_board.cached_threats}")
         else:
             probability = math.exp(-delta_e / T)
-            # print(f"T: {T}, delta_e: {delta_e}, probability: {probability}")
-
+            # print(T, probability)
             if random.random() < probability:
                 next_board = successor_board
-                # print(f"Picking worse {current_board.cached_threats}")
             else:
                 next_board = current_board
 
-        if next_board.cached_threats == current_board.cached_threats:
-            shoulder_counter += 1
-        else:
-            shoulder_counter = 0
-
         # Only updates when state is changed
         if current_board != next_board:
-            traversed_states += 1
+            search_result.traversed_states += 1
             search_result.h_values.append(current_board.cached_threats)
 
         current_board = next_board
 
+    search_result.h_values.append(current_board.cached_threats)
     search_result.board = current_board
     search_result.time_taken = time.time() - t0
-    search_result.traversed_states = traversed_states
     return search_result
